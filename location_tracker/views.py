f6e73cc244a571081f32a7a2ad02bb7dcb4c9fe5
Ethan McCue <emccue@live.com>
2016-10-11 15:19:22 +0000

    commented a little

--- location_tracker/views.py
+++ location_tracker/views.py
@@ -4,8 +4,14 @@ from location_tracker.models import Location
 
 
 location_tracker = Blueprint('location-tracker')
-location_tracker_api = Api(location_tracker)
 
-location_tracker_api.add_resource(Location, '/ethans_location')
+######################################################################
+# We initialize the Api object using the private convention because  #
+# we should only ever reference the Blueprint object outside of this #
+# module, and not the Api directly                                   #
+######################################################################
+_API = Api(location_tracker)
+
+_API.add_resource(Location,'/ethans_location/<float:latitude><float:longitude>')
 
 
